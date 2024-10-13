ebf3f2f2a8bea53ff759a3914b0b9448333e35b6
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-13 20:50:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matematika
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a;
+            double b;
+            double c;
+            double d;
+
+            Console.WriteLine("Unesi varijablu a: ");
+            Console.ReadLine(a, Convert.ToDouble);
+
+            Console.WriteLine("Unesi varijablu b: ");
+            Console.ReadLine(b, Convert.ToDouble);
+
+            Console.WriteLine("Unesi varijablu c: ");
+            Console.ReadLine(c, Convert.ToDouble);
+
+            Console.WriteLine("Unesi varijablu d: ");
+            Console.ReadLine(d, Convert.ToDouble);
+
+            double x = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2) + Math.Pow(d, 2)) /
+                       (Math.Pow(a + b, 2) + Math.Pow(c + d, 2));
+
+            Console.WriteLine("Value of x: " + x);
+            Console.ReadKey();
+        }
+    }
+
+
+}
